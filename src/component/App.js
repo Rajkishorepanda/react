1bb0ef7e5d841d10f47e591dc6ed68e586bd52cc
Rajkishorepanda <rajkishorecvrce@gmail.com>
2024-05-15 23:27:56 +0530

    first commit

---
+++ src/component/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import Header from './header'
+import Contactcard from './contactCard'
+import AddContact from './addContact'
+import ContactList from './contactList'
+
+
+function App() {
+  return (
+   
+
+    <div>
+<Header/>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/header.js
@@ -0,0 +1,16 @@
+import react from 'react'
+
+const Header =()=>{
+    return(
+      <div className='ui fixed menu'>
+        <div className='ui container center'>
+            <h2>This is Header from header</h2>
+        </div>
+      </div>
+
+    );
+
+   
+}
+
+export default Header;
